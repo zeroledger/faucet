3c0f002cf1d49c6d7dbedf4cdee953316f3c631f
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2025-07-01 21:01:00 +0100

    lint

--- jest-e2e.config.ts
+++ jest-e2e.config.ts
@@ -1,25 +1,25 @@
-import type { Config } from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: '.',
-  testEnvironment: 'node',
-  testRegex: '.e2e-spec.ts$',
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: ".",
+  testEnvironment: "node",
+  testRegex: ".e2e-spec.ts$",
   transform: {
-    '^.+\\.(t|j)s?$': '@swc/jest',
+    "^.+\\.(t|j)s?$": "@swc/jest",
   },
-  coverageDirectory: './coverage-e2e',
+  coverageDirectory: "./coverage-e2e",
   coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '<rootDir>/src/(.+)/dto/(.+)',
-    '<rootDir>/src/(.+)/(.+).module.ts',
-    '<rootDir>/src/(.+).module.ts',
-    '<rootDir>/src/main.ts',
-    '<rootDir>/src/run.ts',
-    '<rootDir>/src/config/',
-    '<rootDir>/src/common/db/migrations/',
+    "/node_modules/",
+    "<rootDir>/src/(.+)/dto/(.+)",
+    "<rootDir>/src/(.+)/(.+).module.ts",
+    "<rootDir>/src/(.+).module.ts",
+    "<rootDir>/src/main.ts",
+    "<rootDir>/src/run.ts",
+    "<rootDir>/src/config/",
+    "<rootDir>/src/common/db/migrations/",
   ],
-  modulePaths: ['<rootDir>', '<rootDir>/src'],
+  modulePaths: ["<rootDir>", "<rootDir>/src"],
 };
 
 export default config;
--- jest.config.ts
+++ jest.config.ts
@@ -1,26 +1,26 @@
-import type { Config } from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
   verbose: true,
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  testRegex: '.*\\.spec\\.ts$',
+  moduleFileExtensions: ["js", "json", "ts"],
+  testRegex: ".*\\.spec\\.ts$",
   transform: {
-    '^.+\\.(t|j)s$': '@swc/jest',
+    "^.+\\.(t|j)s$": "@swc/jest",
   },
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/**/*.module.ts'],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/**/*.module.ts"],
   coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '<rootDir>/src/(.+)/dto/(.+)',
-    '<rootDir>/src/(.+)/(.+).module.ts',
-    '<rootDir>/src/(.+).module.ts',
-    '<rootDir>/src/main.ts',
-    '<rootDir>/src/run.ts',
-    '<rootDir>/src/config/',
-    '<rootDir>/src/common/db/migrations/',
+    "/node_modules/",
+    "<rootDir>/src/(.+)/dto/(.+)",
+    "<rootDir>/src/(.+)/(.+).module.ts",
+    "<rootDir>/src/(.+).module.ts",
+    "<rootDir>/src/main.ts",
+    "<rootDir>/src/run.ts",
+    "<rootDir>/src/config/",
+    "<rootDir>/src/common/db/migrations/",
   ],
-  coverageDirectory: '<rootDir>/coverage',
-  testEnvironment: 'node',
-  modulePaths: ['<rootDir>', '<rootDir>/src'],
+  coverageDirectory: "<rootDir>/coverage",
+  testEnvironment: "node",
+  modulePaths: ["<rootDir>", "<rootDir>/src"],
 };
 
 export default config;
